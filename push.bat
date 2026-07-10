2051c1f11ce1101b01e67e6d08379c396d127c7c
s-satou@zeno.or.jp <s-satou@zeno.or.jp>
2026-07-10 13:37:26 +0900

    Update

---
+++ push.bat
@@ -0,0 +1,23 @@
+@echo off
+echo =========================================
+echo   Starting GitHub Push Process
+echo =========================================
+echo.
+
+git add .
+
+set /p msg="Enter commit message (Press Enter for 'Update'): "
+if "%msg%"=="" set msg=Update
+
+echo.
+git commit -m "%msg%"
+echo.
+
+echo Pushing to GitHub...
+git push
+
+echo.
+echo =========================================
+echo   Process Completed.
+echo =========================================
+pause
